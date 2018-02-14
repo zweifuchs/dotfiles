3a400adafc5642bafd4013bb9e750594e9c1ca12
Alfred Eichenseher <alfred.eichenseher@googlemail.com>
2018-02-14 12:17:06 +0100

    minor tweaks to createdb

--- mkutil/mkutil/creatdb.sh
+++ mkutil/mkutil/creatdb.sh
@@ -3,10 +3,23 @@
 EXPECTED_ARGS=4
 E_BADARGS=65
 MYSQL=`which mysql`
-  
-Q1="CREATE DATABASE IF NOT EXISTS $2;"
-Q2="GRANT USAGE ON *.* TO $3@localhost IDENTIFIED BY '$4';"
-Q3="GRANT ALL PRIVILEGES ON $2.* TO $3@localhost;"
+
+RANDP="$(openssl rand -base64 12)"
+ROOTPASS=$1
+DBNAME=$2
+USER=$3
+PASS=$4
+
+if [ "$#" -eq 2 ]; then
+	USER=$2
+	PASS=$RANDP
+	EXPECTED_ARGS=2
+fi
+
+
+Q1="CREATE DATABASE IF NOT EXISTS $DBNAME;"
+Q2="GRANT USAGE ON *.* TO $USER@localhost IDENTIFIED BY '$PASS';"
+Q3="GRANT ALL PRIVILEGES ON $DBNAME.* TO $USER@localhost;"
 Q4="FLUSH PRIVILEGES;"
 SQL="${Q1}${Q2}${Q3}${Q4}"
   
@@ -17,4 +30,5 @@ then
 fi
   
 $MYSQL -vv -uroot -p$1 -e "$SQL"
-
+echo "FINISHED"
+echo "Created DB:$DBNAME with User:$USER and Password:$PASS"
